b262453ee7f8192bc82044deec35c7390af23937
Jakob96 <jakob96@outlook.com>
2021-02-22 21:15:19 +0100

    Added migrations for movies and reviews tables

---
+++ database/migrations/2021_02_22_173940_create_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 100);
+            $table->string('review_text', 1000);
+            $table->integer('rating');
+            $table->foreignId('movie_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}

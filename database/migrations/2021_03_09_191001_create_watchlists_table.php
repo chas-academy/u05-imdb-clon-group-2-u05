3b30249d625687160733f719abe9d65602f5779d
Jakob96 <jakob96@outlook.com>
2021-03-09 20:18:12 +0100

    Added migration for watchlists

---
+++ database/migrations/2021_03_09_191001_create_watchlists_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWatchlistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('watchlists', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('movie_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('watchlists');
+    }
+}

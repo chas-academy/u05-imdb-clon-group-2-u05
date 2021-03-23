5dffc2b0369a990e006216a4bea241d2ee845799
Henrik Frohm <woxer123@gmail.com>
2021-03-23 17:24:02 +0100

    footer created

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -278,8 +278,39 @@
                     </div>
                 </div>
             </div>     
+        </div> 
+    </div>
 
-            
-            </div> 
-</div>
+<footer class="footer bg-black relative pt-1 border-b-2 border-blue-700 mt-8 text-center">
+    <div class="container mx-auto px-6">
+
+        <div class="sm:flex sm:mt-8">
+            <div class="mt-8 sm:mt-0 sm:w-full sm:px-8 flex flex-col md:flex-row justify-between">
+                <div class="flex flex-col">
+                    <span class="font-bold text-gray-500 uppercase mb-2">Features</span>
+                    <span class="my-2"><a href="#" class="text-white-700  text-md hover:text-blue-500">link 1</a></span>
+                    <span class="my-2"><a href="#" class="text-white-700  text-md hover:text-blue-500">link 1</a></span>
+                    <span class="my-2"><a href="#" class="text-white-700  text-md hover:text-blue-500">link 1</a></span>
+                </div>
+                <div class="flex flex-col">
+                    <span class="font-bold text-gray-500 uppercase mt-4 md:mt-0 mb-2">About</span>
+                    <span class="my-2"><p class="text-white text-md">"project description"</p></span>
+                </div>
+                <div class="flex flex-col">
+                    <span class="font-bold text-gray-500 uppercase mt-4 md:mt-0 mb-2">Resources</span>
+                    <span class="my-2"><a href="#" class="text-white text-md hover:text-blue-500">Github</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container mx-auto px-6">
+        <div class="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
+            <div class="sm:w-2/3 text-center py-6">
+                <p class="text-sm text-white font-bold mb-2">
+                    © 2021 by u05-group-2
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
 @endsection

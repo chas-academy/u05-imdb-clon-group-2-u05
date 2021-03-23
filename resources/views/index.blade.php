706bff2f5acbd143da5880f34a0a586ff2965ae3
Henrik Frohm <woxer123@gmail.com>
2021-03-23 21:27:30 +0100

    images added for pop-movies

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
 	<div class="carousel-inner relative overflow-hidden w-full">
 
 		<input class="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden="" checked="checked">
-		<div class="carousel-item absolute opacity-0" style="height:50vh;">
+		<div class="carousel-item absolute opacity-0" style="height:70vh;">
 			<div class="block h-full w-full text-white text-lg text-center font-semibold">Baby Driver
                 <a href="movie">
                     <img src="{{url('/images/BabyDriver.jpg')}}" alt="Image" class="hover:opacity-75 transition ease-in-out duration 150" />
@@ -22,10 +22,10 @@
 		<label for="carousel-2" class="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
 		
 		<input class="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden="">
-		<div class="carousel-item absolute opacity-0" style="height:50vh;">
+		<div class="carousel-item absolute opacity-0" style="height:70vh;">
 			<div class="block h-full w-full text-white text-lg font-semibold text-center">Zack Snyder's Justice League
                 <a href="movie">
-                    <img src="{{url('/images/BabyDriver.jpg')}}" alt="Image" class="hover:opacity-75 transition ease-in-out duration 150" />
+                    <img src="{{url('/images/JusticeLeague.jpg')}}" alt="Image" class="hover:opacity-75 transition ease-in-out duration 150" />
                 </a>
             </div>
 		</div>
@@ -33,10 +33,10 @@
 		<label for="carousel-3" class="next control-2 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label> 
 		
 		<input class="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden="">
-		<div class="carousel-item absolute opacity-0" style="height:50vh;">
+		<div class="carousel-item absolute opacity-0" style="height:70vh;">
 			<div class="block h-full w-full text-white text-lg font-semibold text-center">Lupin
                 <a href="movie">
-                    <img src="{{url('/images/BabyDriver.jpg')}}" alt="Image" class="hover:opacity-75 transition ease-in-out duration 150" />
+                    <img src="{{url('/images/Lupin.jpg')}}" alt="Image" class="hover:opacity-75 transition ease-in-out duration 150" />
                 </a>
             </div>
 		</div>
@@ -64,102 +64,102 @@
         <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
             <div class="mt-8">
                 <a href="movie">
-                    <img src="{{url('/images/BabyDriver.jpg')}}" alt="Image" class="hover:opacity-75 transition ease-in-out duration 150" />
+                    <img src="{{url('/images/Avengers.jpg')}}" alt="Image" class="hover:opacity-75 transition ease-in-out duration 150" />
                 </a>
                 <div class="mt-3">
-                    <a href="movie" class="text-lg mt-2 hover:text-gray:300">Baby Driver</a>
+                    <a href="movie" class="text-lg mt-2 hover:text-gray:300">Avengers: Endgame</a>
                     <div class="flex items-center text-gray-400 text-sm mt-1">
                         <span>
                             <a href="#"></a>
                         <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"/></g></svg> 
                         </span>
-                        <span class="ml-1">7,6</span>
+                        <span class="ml-1">8,4</span>
                         <span class="mx-2">|</span>
-                        <span> July 26, 2017</span>
+                        <span> April 24, 2019</span>
                     </div>
                     <div class="text-gray-400 text-sm">
-                        Action, Crime, Drama 
+                        Action, Adventure, Drama 
                     </div>
                 </div>
             </div>
                         
             <div class="mt-8">
                 <a href="movie">
-                    <img src="{{url('/images/BabyDriver.jpg')}}" alt="Image" class="hover:opacity-75 transition ease-in-out duration 150" />
+                    <img src="{{url('/images/BehindHerEyes.jpg')}}" alt="Image" class="hover:opacity-75 transition ease-in-out duration 150" />
                 </a>
                 <div class="mt-2">
-                    <a href="movie" class="text-lg mt-2 hover:text-gray:300">Baby Driver</a>
+                    <a href="movie" class="text-lg mt-2 hover:text-gray:300">Behind Her Eyes</a>
                     <div class="flex items-center text-gray-400 text-sm mt-1">
                         <span>
                             <a href="#"></a>
                         <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"/></g></svg> 
                         </span>
-                        <span class="ml-1">7,6</span>
+                        <span class="ml-1">7,2</span>
                         <span class="mx-2">|</span>
-                        <span> July 26, 2017</span>
+                        <span> Februari 17, 2021</span>
                     </div>
                     <div class="text-gray-400 text-sm">
-                        Action, Crime, Drama 
+                        Drama, Mystery, Thriller 
                     </div>
                 </div>
             </div>    
                     
             <div class="mt-8">
                 <a href="movie">
-                    <img src="{{url('/images/BabyDriver.jpg')}}" alt="Image" class="hover:opacity-75 transition ease-in-out duration 150" />
+                    <img src="{{url('/images/Coming2America.jpg')}}" alt="Image" class="hover:opacity-75 transition ease-in-out duration 150" />
                 </a>
                 <div class="mt-2">
-                    <a href="movie" class="text-lg mt-2 hover:text-gray:300">Baby Driver</a>
+                    <a href="movie" class="text-lg mt-2 hover:text-gray:300">Coming 2 America</a>
                     <div class="flex items-center text-gray-400 text-sm mt-1">
                         <span>
                             <a href="#"></a>
                         <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"/></g></svg> 
                         </span>
-                        <span class="ml-1">7,6</span>
+                        <span class="ml-1">5,4</span>
                         <span class="mx-2">|</span>
-                        <span> July 26, 2017</span>
+                        <span>  March 5, 2021</span>
                     </div>
                     <div class="text-gray-400 text-sm">
-                        Action, Crime, Drama 
+                        Comedy 
                     </div>
                 </div>
             </div>  
 
             <div class="mt-8">
                 <a href="movie">
-                    <img src="{{url('/images/BabyDriver.jpg')}}" alt="Image" class="hover:opacity-75 transition ease-in-out duration 150" />
+                    <img src="{{url('/images/Soul.jpg')}}" alt="Image" class="hover:opacity-75 transition ease-in-out duration 150" />
                 </a>
                 <div class="mt-2">
-                    <a href="movie" class="text-lg mt-2 hover:text-gray:300">Baby Driver</a>
+                    <a href="movie" class="text-lg mt-2 hover:text-gray:300">Soul</a>
                     <div class="flex items-center text-gray-400 text-sm mt-1">
                         <span>
                             <a href="#"></a>
                         <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"/></g></svg> 
                         </span>
-                        <span class="ml-1">7,6</span>
+                        <span class="ml-1">8,1</span>
                         <span class="mx-2">|</span>
-                        <span> July 26, 2017</span>
+                        <span> December 25, 2020</span>
                     </div>
                     <div class="text-gray-400 text-sm">
-                        Action, Crime, Drama 
+                        Animation, Adventure, Comedy
                     </div>
                 </div>
             </div>
 
             <div class="mt-8">
                 <a href="movie">
-                    <img src="{{url('/images/BabyDriver.jpg')}}" alt="Image" class="hover:opacity-75 transition ease-in-out duration 150" />
+                    <img src="{{url('/images/Tenet.jpg')}}" alt="Image" class="hover:opacity-75 transition ease-in-out duration 150" />
                 </a>
                 <div class="mt-2">
-                    <a href="movie" class="text-lg mt-2 hover:text-gray:300">Baby Driver</a>
+                    <a href="movie" class="text-lg mt-2 hover:text-gray:300">Tenet</a>
                     <div class="flex items-center text-gray-400 text-sm mt-1">
                         <span>
                             <a href="#"></a>
                         <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"/></g></svg> 
                         </span>
-                        <span class="ml-1">7,6</span>
+                        <span class="ml-1">7,5</span>
                         <span class="mx-2">|</span>
-                        <span> July 26, 2017</span>
+                        <span> August 26, 2020</span>
                     </div>
                     <div class="text-gray-400 text-sm">
                         Action, Crime, Drama 
@@ -279,30 +279,30 @@
         </div> 
     </div>
 
-<footer class="footer bg-black relative pt-1 border-b-2 border-blue-700 mt-8 text-center">
+<footer class="footer bg-black relative pt-1 border-b-2 border-gray-900 mt-8 text-center">
     <div class="container mx-auto px-6">
 
         <div class="sm:flex sm:mt-8">
             <div class="mt-8 sm:mt-0 sm:w-full sm:px-8 flex flex-col md:flex-row justify-between">
                 <div class="flex flex-col">
-                    <span class="font-bold text-gray-500 uppercase mb-2 ">Features</span>
+                    <span class="font-bold text-yellow-500 uppercase mb-2 ">Features</span>
                     <span class="my-2"><a href="#" class="text-white-700  text-md hover:text-blue-500">Design</a></span>
                     <span class="my-2"><a href="#" class="text-white-700  text-md hover:text-blue-500">lorem</a></span>
                     <span class="my-2"><a href="#" class="text-white-700  text-md hover:text-blue-500">lorem</a></span>
                 </div>
                 <div class="flex flex-col">
-                    <span class="font-bold text-gray-500 uppercase mt-4 md:mt-0 mb-2">About</span>
-                    <span class="my-2"><p class="text-white text-md">This is a website designed to replicate popular <br> movie-websites like IMDB and Rotten Tomatoes. <br><br> It was built using Laravel framework, <br>using Tailwind CSS and MySQL-database.</p></span>
+                    <span class="font-bold text-yellow-500 uppercase mt-4 md:mt-0 mb-2">About</span>
+                    <span class="my-2"><p class="text-white text-md ">This is a website designed to replicate popular <br> movie-websites like IMDB and Rotten Tomatoes. <br><br> It was built using Laravel framework, <br>utilizing Tailwind CSS and MySQL-database.</p></span>
                 </div>
                 <div class="flex flex-col">
-                    <span class="font-bold text-gray-500 uppercase mt-4 md:mt-0 mb-2">Resources</span>
+                    <span class="font-bold text-yellow-500 uppercase mt-4 md:mt-0 mb-2">Resources</span>
                     <span class="my-2"><a href="#" class="text-white text-md hover:text-blue-500">Github</a></span>
                 </div>
             </div>
         </div>
     </div>
     <div class="container mx-auto px-6">
-        <div class="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
+        <div class="mt-16 border-t-2 border-gray-900 flex flex-col items-center">
             <div class="sm:w-2/3 text-center py-6">
                 <p class="text-sm text-white font-bold mb-2">
                     © 2021 by u05-group-2

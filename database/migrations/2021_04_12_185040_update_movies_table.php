b289fb357ce24ee5bf34c7c9fa12ecf33b94a1ae
Jakob96 <jakob96@outlook.com>
2021-04-12 22:20:40 +0200

    Renamed due to error msg in artisan

--- database/migrations/2021_04_12_185040_update_movies_table.php
+++ database/migrations/2021_04_12_185040_update_movies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateMoviesTable extends Migration
+class UpdateMoviesTableRating extends Migration
 {
     /**
      * Run the migrations.

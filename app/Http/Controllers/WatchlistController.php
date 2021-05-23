693cde62f3bdfa4fe9a5e0db8dc46045c08ae536
Jakob96 <jakob96@outlook.com>
2021-05-23 22:52:13 +0200

    Add methods for add and remove movies from watchlist

--- app/Http/Controllers/WatchlistController.php
+++ app/Http/Controllers/WatchlistController.php
@@ -30,24 +30,34 @@ class WatchlistController extends Controller
     }
 
     /**
-     * Add movie
+     * Add movie to watchlist
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function addMovie($id)
     {
-        //
+        $listId = Watchlist::where('user_id', Auth::id())->get('id')[0]->id;
+        $movie = new MovieBelongsToList;
+        $movie->watchlist_id = $listId;
+        $movie->movie_id = $id;
+        $movie->save();
+
+        $movieData = Movie::where('id', $id)->firstOrFail();
+        return view('show-movie', ['movie' => $movieData]);
     }
    
     /**
-     * Remove movie
+     * Remove movie from watchlist
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function removeMovie($id)
     {
-        //
+        $listId = Watchlist::where('user_id', Auth::id())->get('id')[0]->id;
+        MovieBelongsToList::where('movie_id', $id)->where('watchlist_id', $listId)->delete();
+
+        return $this->show();
     }
 }

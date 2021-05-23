1f3c9444a21a754fb17996ae32eaa23525a9777d
Jakob96 <jakob96@outlook.com>
2021-05-23 19:13:23 +0200

    Add show method for watchlist

---
+++ app/Http/Controllers/WatchlistController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Watchlist;
+use App\Models\MovieBelongsToList;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
+class WatchlistController extends Controller
+{
+    /**
+     * Display user's list.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $userBelongsToList = Watchlist::where('user_id', Auth::id())->where('id', $id)->exists();
+
+        if ($userBelongsToList) {
+            return $movies = DB::table('movie_belongs_to_list')->join('movies', 'movie_belongs_to_list.movie_id', '=', 'movies.id')->where('watchlist_id', $id)->select('movie_belongs_to_list.movie_id', 'movies.thumbnail', 'movies.name', 'movies.genre', 'movie_belongs_to_list.created_at')->get();
+            return view('watchlist', ['movies' => $movies]);
+        }
+        else {
+            return response(view('errors.403'),403);
+        }
+    }
+
+    /**
+     * Add movie
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function addMovie($id)
+    {
+        //
+    }
+   
+    /**
+     * Remove movie
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function removeMovie($id)
+    {
+        //
+    }
+}

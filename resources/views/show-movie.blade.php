80ade2b11bdc5cbd39536e9196f86ff1394de1e4
Jakob96 <jakob96@outlook.com>
2021-05-24 22:44:11 +0200

    Add movie data

--- resources/views/show-movie.blade.php
+++ resources/views/show-movie.blade.php
@@ -9,11 +9,11 @@
                             <a href="#"></a>
                         <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"/></g></svg> 
                         </span>
-                        <span class="ml-1">7,6</span>
+                        <span class="ml-1">{{ $movie->rating }}</span>
                         <span class="mx-2">|</span>
-                        <span> July 26, 2017</span>
+                        <span>{{ $movie->year }}</span>
                         <span class="mx-2">|</span>
-                        <span>Action, Crime, Drama</span>
+                        <span>{{ $movie->genre }}</span>
                     </div>
 
                     <p class="text-gray-300 mt-8">
